d239c9c1d948a76ad589e1519ec6545fabc6cad7
mayank mahadevwala <itsmemyk@gmail.com>
2017-01-11 14:36:20 +0530

    style() : well structure objects

--- src/main/java/com/axelor/app/MainApp.java
+++ src/main/java/com/axelor/app/MainApp.java
@@ -4,6 +4,13 @@ package com.axelor.app;
 public class MainApp {
     
     public static void main ( String [] args ) {
+    	Task junit = new Task("JUnit", "Create one demo java Application for jUnit Testing");
+    	Task slf4j = new Task("Slf4j", "Create one demo java Application to make custom logging system");
+    	Task git = new Task("Git", "Create single app, maintain different programs in different branch");
+    	Task eclipse = new Task("Eclipse", "Kindly check all custom settings for eclipse project");
+    	Task joiningReport = new Task("Project Report", "Kindly submit all project reports to college");
+    	Task gradle = new Task("Gradle", "Create java app using gradle [Hint: apply dependency]");
+    	Task training = new Task("Training", "Teaching to axelor team");
     	
 		Trainee agaAxelor = new Trainee("Akshay", "aga");
 		Trainee jsoAxelor = new Trainee("Jai", "jso");
@@ -11,19 +18,18 @@ public class MainApp {
 		Trainee tboAxelor = new Trainee("Tushar", "tbo");
 		Trainee mmahAxelor = new Trainee("Mayank", "mmah");
 		
-		agaAxelor.assign(new Task("JUnit", "Create one demo java Application for jUnit Testing"));
-		agaAxelor.assign(new Task("Slf4j", "Create one demo java Application to make custom logging system"));
-		agaAxelor.assign(new Task("Git", "Create single app, maintain different programs in different branch"));
+		agaAxelor.assign(junit);
+		agaAxelor.assign(slf4j);
+		agaAxelor.assign(git);
 
 		agaAxelor.completeAll();
 		
-		jsoAxelor.assign(new Task("Eclipse", "Kindly check all custom settings for eclipse project"));
+		jsoAxelor.assign(eclipse);		
+		ugoAxelor.assign(joiningReport);		
+		tboAxelor.assign(gradle);
 		
-		ugoAxelor.assign(new Task("Project Report", "Kindly submit all project reports to college"));
-		
-		tboAxelor.assign(new Task("Gradle", "Create java app using gradle [Hint: apply dependency]"));
-		
-		mmahAxelor.assign(new Task("Training", "Teaching to axelor team"));
+		mmahAxelor.assign(training);
+		mmahAxelor.complete(training);
 		
     	Team axelor= new Team("Axelor Trainees");
     	axelor.addMember(agaAxelor);
